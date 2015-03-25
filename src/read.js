c7d3f285b47ff9876cb30516b0b826564ec8f4dc
Kensei <kendemu@me.com>
2015-03-25 18:43:18 +0900

    rosweb.js developed, need unit test

--- src/read.js
+++ src/read.js
@@ -3,6 +3,7 @@ var topic_path = "../config/rostopic.txt";
 var type_path = "../config/rostype.txt";
 fs.readFile(topic_path,function(err,data){
     var module.exports.topics=data.toString().split('\n');
+    var module.exports.topics_len=data.toString().split.('\n').length -1;
 });
 fs.readFile(topic_path,function(err,data){
     var module.exports.types=data.toString().split('\n');
--- src/rosweb.js
+++ src/rosweb.js
@@ -1,1 +1,41 @@
 var read=require("./read.js");
+
+//Establishing Connection
+
+var ros=new ROSLIB.Ros({
+    url: 'ws://localhost:9090'
+});
+
+ros.on('error', function(error) {
+    console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+   console.log('Connection to websocket server closed.');
+});
+
+//registering ROS topics
+
+var rostopics = [];
+
+for(var i =0; i < read.topics_len; i++){
+    var rostopics[i] = new ROSLIB.Topic({
+	ros:ros,
+	name:read.topics[i],
+	messageType:read.types[i]
+    });
+}
+
+var rosdata = [];
+
+//Subscribing ROS topics
+for(var i =0; i < read.topics_len; i++){
+    rostopics[i].subscribe(function(message){
+	rosdata[i] = message.data;
+    });
+}    
+
+//Republishing ROS topics(websocket packet parsing)
+for(var i=0; i < read.topics_len; i++){
+    rostopics[i].publish(rosdata[i]);
+}
