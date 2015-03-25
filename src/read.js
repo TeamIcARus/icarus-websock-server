a9bd489f5c6a95c8e50ef13606dcebca1ef96266
Kensei <kendemu@me.com>
2015-03-25 18:24:18 +0900

    finished read.js, going to rosweb.js

--- src/read.js
+++ src/read.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var topic_path = "../config/rostopic.txt";
 var type_path = "../config/rostype.txt";
 fs.readFile(topic_path,function(err,data){
-    var topics=data.toString().split('\n');
+    var module.exports.topics=data.toString().split('\n');
 });
 fs.readFile(topic_path,function(err,data){
-    var types=data.toString().split('\n');
+    var module.exports.types=data.toString().split('\n');
 });
