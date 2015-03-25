584253e8ae4bc604f4d4e01dc108e5d763ace9ce
Kensei <kendemu@me.com>
2015-03-26 00:05:51 +0900

    changed rossetup.bash

--- config/rossetup.bash
+++ config/rossetup.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
 rostopic list | tee rostopic.txt
 $(pwd)/rostype.bash rostopic.txt | tee rostype.txt
+echo "topic autoread completed"
+echo "going to convert to rostopic packets"
+node ../src/rosweb.js
