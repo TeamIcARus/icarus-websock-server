2a508a01d8611b143a967970a4b46c3e01efeca6
Kensei <kendemu@me.com>
2015-03-25 19:21:33 +0900

    read.js unit tested, ok

--- src/read.js
+++ src/read.js
@@ -1,12 +1,22 @@
 var fs = require('fs');
 var topic_path = "../config/rostopic.txt";
 var type_path = "../config/rostype.txt";
+console.log("started reading");
+var topics = [];
+var topics_len;
+var types=[];
 fs.readFile(topic_path,function(err,data){
-    var module.exports.topics=data.toString().split('\n');
-    var module.exports.topics_len=data.toString().split.('\n').length -1;
+    topics=data.toString().split('\n');
+    topics_len=data.toString().split('\n').length -1;
     console.log("reading"+topic_path);
 });
 fs.readFile(type_path,function(err,data){
-    var module.exports.types=data.toString().split('\n');
+    exports.types=data.toString().split('\n');
     console.log("reading"+type_path);
 });
+
+module.exports = {
+  topics : topics,
+  topics_len :topics_len,
+  types : types
+};
