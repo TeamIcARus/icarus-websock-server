a999e5d480fd8284579a118d13775aab16edbefc
Kensei <kendemu@me.com>
2015-03-28 12:10:35 +0900

    client-server system server side finished

--- src/browser-exec/client/client.js
+++ src/browser-exec/client/client.js
@@ -1,6 +1,35 @@
+var topic_data,type_data;
+var topics = [], type =[], topics_len, type_len;
+
 window.onload = function() {
-    socket = new WebSocket("ws://192.168.12.50:10080");
-    socket.onmessage = function(event) {
-	document.getElementById('news').innerHTML = event.data;
+    socket_topic = new WebSocket("ws://localhost:10080");
+    socket_type = new WebSocket("ws://localhost:10081");
+    socket_topic.onmessage = function(event) {
+	topic_data= event.data.split("\n");
+	topic_len = topic_data.length;
+	for(var i = 0; i < topic_len; i++){
+	    topics = topic_data[i];
+	}
     }
+    socket_type.onmessage=function(event){
+	type_data = event.data.split("\n");
+	type_len = event.data.split("\n");
+	for(var i = 0; i< type_len; i++){
+	    type = type_data[i];
+	}
+    }
+    var ros = new ROSLIB.Ros({
+	url:"ws://localhost:9090"
+    });
+    
+    ros.on("connection",function(){
+	console.log("Connected to websocket server.");
+    });
+    ros.on('error', function(error) {
+	console.log('Error connecting to websocket server: ', error);
+    });
+    
+    ros.on('close', function() {
+    console.log('Connection to websocket server closed.');
+  });
 }
